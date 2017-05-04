6329b37b2f76cd33d385a2b7369987bb94fe3f2b
chuanxingzhang <chuanxingzhang@126.com>
2017-05-04 10:06:19 +0800

    update

--- src/main/java/com/lp/db/MysqlDataType.java
+++ src/main/java/com/lp/db/MysqlDataType.java
@@ -2,123 +2,126 @@ package com.lp.db;
 
 /**
  * mysql数据类型，java数据类型，ibatis JDBCTYPE，之间的对应关系
- *
  */
 public enum MysqlDataType {
-	
-	/**
-	 * 
-	 */
-	BIGINT("bigint", "Long", "BIGINT","java.lang.Long"),
-	/**
-	 * 
-	 */
-	BINARY("binary", "byte[]", "BINARY","java.lang.Byte"),
-	/**
-	 * 
-	 */
-	BIT("bit", "byte[]", "BIT","java.lang.Byte"),
-	/**
-	 * 
-	 */
-	BLOB("blob", "byte[]", "BLOB","java.lang.Byte"),
-	/**
-	 * 
-	 */
-	CHAR("char", "String", "CHAR","java.lang.String"),
-	/**
-	 *
-	 */
-	DATE("date", "Date", "DATE","java.util.Date"),
-	/**
-	 * 
-	 */
-	DATETIME("datetime", "Date", "TIMESTAMP","java.util.Date"),
-	/**
-	 * 
-	 */
-	DECIMAL("decimal", "BigDecimal", "DECIMAL","java.math.BigDecimal"),
-	/**
-	 * 
-	 */
-	DOUBLE("double", "Double", "DOUBLE","java.lang.Double"),
-	/**
-	 * 
-	 */
-	FLOAT("float", "Float", "FLOAT","java.lang.Float"),
-	/**
-	 * 
-	 */
-	INT("int", "Integer", "INTEGER","java.lang.Integer"),
-	/**
-	 * 
-	 */
-	SMALLINT("smallint", "Short", "SMALLINT","java.lang.Short"),
-	/**
-	 * 
-	 */
-	TEXT("text", "String", "LONGVARCHAR","java.lang.String"),
-	/**
-	 * 
-	 */
-	TIME("time", "Date", "TIME","java.util.Date"),
-	/**
-	 * 
-	 */
-	TIMESTAMP("timestamp", "Date", "TIMESTAMP","java.util.Date"),
-	/**
-	 * 
-	 */
-	TINYINT("tinyint", "Byte", "TINYINT","java.lang.Byte"),
-	/**
-	 * 
-	 */
-	VARCHAR("varchar", "String", "VARCHAR","java.lang.String");
-	
-	private String mySqlDataType;
-	
-	private String javaDataType;
-	
-	private String ibatisJdbcType;
+    /**
+     *
+     */
+    MEDIUMINT("mediumint", "Integer", "INTEGER", "java.lang.Integer"),
 
-	private String primaryKeyIbatisJavaType;
-	
-	MysqlDataType(String mySqlDataType, String javaDataType, String ibatisJdbcType,String primaryKeyIbatisJavaType) {
-		this.mySqlDataType = mySqlDataType;
-		this.javaDataType = javaDataType;
-		this.ibatisJdbcType = ibatisJdbcType;
-		this.primaryKeyIbatisJavaType=primaryKeyIbatisJavaType;
-	}
+    /**
+     *
+     */
+    BIGINT("bigint", "Long", "BIGINT", "java.lang.Long"),
+    /**
+     *
+     */
+    BINARY("binary", "byte[]", "BINARY", "java.lang.Byte"),
+    /**
+     *
+     */
+    BIT("bit", "byte[]", "BIT", "java.lang.Byte"),
+    /**
+     *
+     */
+    BLOB("blob", "byte[]", "BLOB", "java.lang.Byte"),
+    /**
+     *
+     */
+    CHAR("char", "String", "CHAR", "java.lang.String"),
+    /**
+     *
+     */
+    DATE("date", "Date", "DATE", "java.util.Date"),
+    /**
+     *
+     */
+    DATETIME("datetime", "Date", "TIMESTAMP", "java.util.Date"),
+    /**
+     *
+     */
+    DECIMAL("decimal", "BigDecimal", "DECIMAL", "java.math.BigDecimal"),
+    /**
+     *
+     */
+    DOUBLE("double", "Double", "DOUBLE", "java.lang.Double"),
+    /**
+     *
+     */
+    FLOAT("float", "Float", "FLOAT", "java.lang.Float"),
+    /**
+     *
+     */
+    INT("int", "Integer", "INTEGER", "java.lang.Integer"),
+    /**
+     *
+     */
+    SMALLINT("smallint", "Short", "SMALLINT", "java.lang.Short"),
+    /**
+     *
+     */
+    TEXT("text", "String", "LONGVARCHAR", "java.lang.String"),
+    /**
+     *
+     */
+    TIME("time", "Date", "TIME", "java.util.Date"),
+    /**
+     *
+     */
+    TIMESTAMP("timestamp", "Date", "TIMESTAMP", "java.util.Date"),
+    /**
+     *
+     */
+    TINYINT("tinyint", "Byte", "TINYINT", "java.lang.Byte"),
+    /**
+     *
+     */
+    VARCHAR("varchar", "String", "VARCHAR", "java.lang.String");
 
-	public String getMySqlDataType() {
-		return mySqlDataType;
-	}
+    private String mySqlDataType;
 
-	public String getJavaDataType() {
-		return javaDataType;
-	}
+    private String javaDataType;
 
-	public String getIbatisJdbcType() {
-		return ibatisJdbcType;
-	}
+    private String ibatisJdbcType;
 
-	public void setMySqlDataType(String mySqlDataType) {
-		this.mySqlDataType = mySqlDataType;
-	}
+    private String primaryKeyIbatisJavaType;
 
-	public void setJavaDataType(String javaDataType) {
-		this.javaDataType = javaDataType;
-	}
+    MysqlDataType(String mySqlDataType, String javaDataType, String ibatisJdbcType, String primaryKeyIbatisJavaType) {
+        this.mySqlDataType = mySqlDataType;
+        this.javaDataType = javaDataType;
+        this.ibatisJdbcType = ibatisJdbcType;
+        this.primaryKeyIbatisJavaType = primaryKeyIbatisJavaType;
+    }
 
-	public void setIbatisJdbcType(String ibatisJdbcType) {
-		this.ibatisJdbcType = ibatisJdbcType;
-	}
+    public String getMySqlDataType() {
+        return mySqlDataType;
+    }
 
-	public String getPrimaryKeyIbatisJavaType() {
-		return primaryKeyIbatisJavaType;
-	}
+    public String getJavaDataType() {
+        return javaDataType;
+    }
 
-	public void setPrimaryKeyIbatisJavaType(String primaryKeyIbatisJavaType) {
-		this.primaryKeyIbatisJavaType = primaryKeyIbatisJavaType;
-	}
+    public String getIbatisJdbcType() {
+        return ibatisJdbcType;
+    }
+
+    public void setMySqlDataType(String mySqlDataType) {
+        this.mySqlDataType = mySqlDataType;
+    }
+
+    public void setJavaDataType(String javaDataType) {
+        this.javaDataType = javaDataType;
+    }
+
+    public void setIbatisJdbcType(String ibatisJdbcType) {
+        this.ibatisJdbcType = ibatisJdbcType;
+    }
+
+    public String getPrimaryKeyIbatisJavaType() {
+        return primaryKeyIbatisJavaType;
+    }
+
+    public void setPrimaryKeyIbatisJavaType(String primaryKeyIbatisJavaType) {
+        this.primaryKeyIbatisJavaType = primaryKeyIbatisJavaType;
+    }
 }
